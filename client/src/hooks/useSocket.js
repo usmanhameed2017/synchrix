3f23b080133afebfeffaf5c0fb79e3db667c164a
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-10-08 12:01:19 +0500

    Created a custom socket hook

---
+++ client/src/hooks/useSocket.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import socket from "../service/socket";
+
+function useSocket(event, callback)
+{
+    useEffect(() => {
+        if(!socket || !event || typeof callback !== "function") return;
+
+        // Listen for event
+        socket?.on(event, callback);
+
+        // Cleanup on unmount
+        return () => socket?.off(event, callback);
+    }, [event, callback]);
+}
+
+export default useSocket;
