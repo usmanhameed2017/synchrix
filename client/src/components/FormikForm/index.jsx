a8de1bc2e8ffa080cc1238c4fc7819bd9d9f262e
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-10-25 16:21:11 +0500

    Fix: Refactor formik form component with forward ref

--- client/src/components/FormikForm/index.jsx
+++ client/src/components/FormikForm/index.jsx
@@ -1,26 +1,35 @@
 import { Formik, Form } from 'formik';
-import styles from "./style.module.css";
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 
-function FormikForm({ children, initialValues, validationSchema, handlerFunction, className, heading })
-{
+const FormikForm = forwardRef(({ children, initialValues, validationSchema, handlerFunction,
+    validateOnBlur = true, validateOnChange = true,  className }, ref) => {
+    const formikRef = useRef();
+
+    // Expose Formik submit function to parent
+    useImperativeHandle(ref, () => ({
+        submitForm: () => {
+            if(formikRef.current) formikRef.current.handleSubmit();
+        }
+    }));
+
     return (
-        <div className={styles.formContainer}>
-            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handlerFunction} 
-            enableReinitialize={true} validateOnBlur={false} validateOnChange={false}>
+        <>
+            <Formik
+                innerRef={formikRef}
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={handlerFunction}
+                enableReinitialize={true}
+                validateOnBlur={validateOnBlur}
+                validateOnChange={validateOnChange}>
                 {formikProps => (
-                    <Form className={className ? className : `${styles.form}`}>
-                        {heading && (
-                            <div className={styles.heading}> 
-                                <h3 style={{ letterSpacing:"1px" }}> { heading } </h3> 
-                                <hr />
-                            </div>
-                        )}         
-                        { typeof children === "function" ? children(formikProps) : children }
+                    <Form className={className}>
+                        {typeof children === "function" ? children(formikProps) : children}
                     </Form>
-                )}            
+                )}
             </Formik>
-        </div>
+        </>
     );
-}
+});
 
 export default FormikForm;
