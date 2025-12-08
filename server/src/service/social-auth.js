450db8d72f2ab695fe35341b6084d12a494f9ef4
usmanhameed2017 <usmanhameed1790+backup@gmail.com>
2025-12-08 16:12:48 +0500

    Create modular service for google OAuth

---
+++ server/src/service/social-auth.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/user');
+
+// Google
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+}, 
+async (accessToken, refreshToken, profile, done) => {
+    try 
+    {
+        // Extract properties
+        const gid = profile?.id;
+        const name = `${profile?.name?.givenName} ${profile?.name?.familyName}`;
+        const email = profile?.emails?.[0]?.value;
+        const username = profile?.name?.givenName;
+
+        // If user already exist in database
+        const existingUser = await User.findOne({ $or:[{ gid:gid }, { email:email }] });
+        if(existingUser) return done(null, existingUser);
+    
+        // Create new user
+        const createUser = await User.create({
+            gid:gid,
+            name:name,
+            email:email,
+            username:username,
+            password:`GoogleAccount:${gid}`
+        });
+        return done(null, createUser);
+    } 
+    catch(error) 
+    {
+        return done(error, null);
+    }
+}));
