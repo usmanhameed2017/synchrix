c8da9aa9d04a0ba73716af148ce81a39ecadcf3c
usmanhameed2017 <usmanhameed1790+backup@gmail.com>
2025-12-08 16:30:49 +0500

    Integrated google login button component

---
+++ client/src/components/GoogleLoginButton/index.jsx
@@ -0,0 +1,19 @@
+import { backendURL } from "../../constants";
+import styles from "./style.module.css";
+
+function GoogleLoginButton() 
+{
+    // Public url of google login button
+    const src = "https://developers.google.com/identity/images/g-logo.png";
+    return (
+        <a href={`${backendURL}/api/v1/user/auth/google`}>
+            <button type='button' className={styles.googleBtn}>
+                <img src={src} alt="Google Logo" className={styles.image} />
+                Sign in with Google
+            </button>
+        </a>
+
+    );
+}
+
+export default GoogleLoginButton;
