9adf54df7e245dcda0dc5fa77f5fa7297571a357
usmanhameed2017 <usmanhameed1790+backup@gmail.com>
2025-12-08 13:35:51 +0500

    Add custom validation to user model

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -2,38 +2,12 @@ const { Schema, model } = require("mongoose");
 
 // Schema
 const userSchema = new Schema({
-    name:{
-        type:String,
-        trim:true,
-        required:true
-    },
-    email:{
-        type:String,
-        trim:true,
-        lowercase:true,
-        unique:true,
-        required:true,       
-    },
-    username:{
-        type:String,
-        trim:true,
-        lowercase:true,
-        unique:true,
-        index:true,
-        required:true
-    },
-    password:{
-        type:String,
-        trim:true,
-        required:true
-    },
-    onlineStatus:{
-        type:String,
-        trim:true,
-        enum:["Online", "Offline"],
-        default:"Offline",
-        required:true
-    }
+    gid:{ type:String, trim:true },
+    name:{ type:String, trim:true, required:[true, "Name is required"] },
+    email:{ type:String, trim:true, lowercase:true, unique:[true, "This email has already taken. Please try different email address"], required:true },
+    username:{ type:String, trim:true, lowercase:true, unique:[true, "This username is already in use"], index:true, required:[true, "Username is required"] },
+    password:{ type:String, trim:true, required:[true, "Password is required"] },
+    onlineStatus:{ type:String, trim:true, enum:["Online", "Offline"], default:"Offline", required:true }
 }, { timestamps:true });
 
 // Model
