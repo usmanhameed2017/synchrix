565e2df5cd19728e4a1b9693dd7c8b6b9793179f
usmanhameed2017 <usmanhameed1790+backup@gmail.com>
2025-12-09 15:10:20 +0500

    Add: facebook id

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 // Schema
 const userSchema = new Schema({
     gid:{ type:String, trim:true },
+    fid:{ type:String, trim:true },
     name:{ type:String, trim:true, required:[true, "Name is required"] },
     email:{ type:String, trim:true, lowercase:true, unique:[true, "This email has already taken. Please try different email address"], required:true },
     username:{ type:String, trim:true, lowercase:true, unique:[true, "This username is already in use"], index:true, required:[true, "Username is required"] },
