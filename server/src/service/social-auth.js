3bb4e3ad383f717bd12b6d8a1464b6fd6c484d15
usmanhameed2017 <usmanhameed1790+backup@gmail.com>
2025-12-09 15:56:09 +0500

    Inject facebook strategy for auth

--- server/src/service/social-auth.js
+++ server/src/service/social-auth.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const User = require('../models/user');
 
 // Google
