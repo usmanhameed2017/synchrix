9237d4c7b1a861ca8767acb8311257ab8fcb5f3b
usmanhameed2017 <usmanhameed1790+backup@gmail.com>
2025-12-09 16:16:27 +0500

    Set button type

--- client/src/components/FacebookLoginButton/index.jsx
+++ client/src/components/FacebookLoginButton/index.jsx
@@ -6,7 +6,7 @@ function FacebookLoginButton({ text = "Sign in with Facebook" })
     return (
         <div className='d-grid mt-2'>
             <a href={`${backendURL}/api/v1/auth/facebook`}>
-                <button className={styles.facebookButton}>
+                <button type='button' className={styles.facebookButton}>
                     <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png" alt="Facebook Logo" />
                     {text}
                 </button> 
