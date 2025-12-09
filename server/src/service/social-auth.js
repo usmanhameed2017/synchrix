150fe1d7bf7ace7f770277cdd2831daa21e3a987
usmanhameed2017 <usmanhameed1790+backup@gmail.com>
2025-12-09 15:09:46 +0500

    Add: Custom service utility function for facebook login

--- server/src/service/social-auth.js
+++ server/src/service/social-auth.js
@@ -35,3 +35,36 @@ async (accessToken, refreshToken, profile, done) => {
         return done(error, null);
     }
 }));
+
+// Facebook
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+    profileFields: ["id", "displayName", "emails"]
+}, 
+async (accessToken, refreshToken, profile, done) => {
+    try 
+    {
+        // Extract properties
+        const fid = profile?.id;
+        const name = profile?.displayName;
+        const email = profile?.emails?.[0]?.value;
+        const username = email;
+
+        // Check if user already exists
+        const existingUser = await User.findOne({ $or:[{ fid:fid }, { email:email }] });
+        if(existingUser) return done(null, existingUser);
+
+        // Prepare payload
+        const payload = { fid, name, email, username, password:`FacebookAccount:${fid}`, status:"Approved" };        
+
+        // Create new user
+        const createUser = await User.create(payload);
+        return done(null, createUser);
+    } 
+    catch(error) 
+    {
+        return done(error, null);
+    }
+}));
